679ef685c98deb13d30af7c9cb31bfd78894e340
Jannek <MCelements0815@gmail.com>
2019-12-27 20:32:58 +0100

    more webhook stuff

--- src/main/java/de/jannek/price/tracking/sql/entities/BaseModel.java
+++ src/main/java/de/jannek/price/tracking/sql/entities/BaseModel.java
@@ -1,8 +1,8 @@
 package de.jannek.price.tracking.sql.entities;
 
 import io.ebean.Model;
+import io.ebean.annotation.DbDefault;
 import io.ebean.annotation.WhenCreated;
-import io.ebean.annotation.WhenModified;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Version;
 import java.time.Instant;
 
 /**
@@ -26,13 +25,10 @@ public abstract class BaseModel extends Model {
     @Id
     protected long id;
 
-    @Version
-    protected Long version;
-
     @WhenCreated
     protected Instant whenCreated;
 
-    @WhenModified
-    protected Instant whenModified;
+    @DbDefault(value = "true")
+    protected boolean enabled;
 
 }
