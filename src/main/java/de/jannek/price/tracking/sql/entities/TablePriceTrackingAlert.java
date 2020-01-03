9d0274e5261045a494c74c5af6b783b767e6e493
Jannek <mcelements0815@gmail.com>
2020-01-03 16:07:11 +0100

    added alert table

--- src/main/java/de/jannek/price/tracking/sql/entities/TablePriceTrackingAlert.java
+++ src/main/java/de/jannek/price/tracking/sql/entities/TablePriceTrackingAlert.java
@@ -1,6 +1,6 @@
 package de.jannek.price.tracking.sql.entities;
 
-import io.ebean.annotation.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,33 +8,34 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
  * Created by IntelliJ IDEA.
  * User: Jannek Behrens
- * Timestamp: 19/12/2019 23:07
+ * Timestamp: 03/01/2020 15:47
  */
 
 @Entity
-@Table(name = "price_tracking_data")
+@Table(name = "price_tracking_alert")
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
-public class TablePriceTrackingData extends BaseModel {
+public class TablePriceTrackingAlert extends BaseModel {
 
     @ManyToOne
     private TablePriceTrackingTackedProduct product;
 
-    @NotNull
-    private String type;
-
-    @NotNull
     private double price;
 
-    @OneToOne(mappedBy = "data")
-    private TablePriceTrackingDataRaw rawData;
+    /**
+     * The protocol of the notification e. g. SMS, Mail, ...
+     */
+    private String destinationType;
+    /**
+     * The E-Mail address of the client
+     */
+    private String destination;
 
 }
